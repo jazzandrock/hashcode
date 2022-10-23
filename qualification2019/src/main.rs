79b361a72e8e07bb0510c563c50d702edd1f4ba0
Oleg Syniakevych <olegsyniakevych@gmail.com>
2022-10-23 20:42:56 +0300

    Add another solution with bitsets for D and E, running time dropped to 9 min (still surprisingly long)

--- qualification2019/src/main.rs
+++ qualification2019/src/main.rs
@@ -20,6 +20,8 @@ use std::writeln;
 use std::io::Read;
 use helpers::red::Red;
 
+use bit_set::BitSet;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
@@ -146,6 +148,12 @@ fn get_score(a: &[u32], b: &[u32]) -> usize {
     std::cmp::min(common, not_common)
 }
 
+fn get_score_bits(a: &BitSet, b: &BitSet) -> usize {
+    let common = a.intersection(b).count();
+    let not_common = std::cmp::min(a.len() - common, b.len() - common);
+    std::cmp::min(common, not_common)
+}
+
 fn outer_join(a: &[u32], b: &[u32]) -> Vec<u32> {
     let mut result = Vec::with_capacity(a.len() + b.len());
 
@@ -244,6 +252,107 @@ fn solve(in_file: &str, out_file: &str) -> Result<(), Box<dyn std::error::Error>
                 );
 
                 let score = get_score(&prev_slide_tags, &curr_slide_tags);
+                best_image = std::cmp::max(best_image, (score, j as u32));
+            }
+
+            let (score, j) = best_image;
+            if j == NON_EXISTENT_IMG {
+                break;
+            }
+
+            solution.push((first_best_vertical_img_id, j));
+            images[first_best_vertical_img_id as usize].used = true;
+            images[j as usize].used = true;
+
+            total_score += score;
+        }
+    }
+    println!("{} total score: {}", in_file, total_score);
+
+    Ok(())
+}
+
+fn solve_bits(in_file: &str, out_file: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let Input { n_images, all_tags, mut images } = read_problem(in_file);
+    
+    let use_bitsets = all_tags.len() <= 500;
+    let mut bit_tags = Vec::<BitSet>::new();
+    if use_bitsets {
+        bit_tags.reserve_exact(n_images as usize);
+        for img in &images {
+            let mut bits = BitSet::with_capacity(all_tags.len());
+            for &tag in &img.tags {
+                bits.insert(tag as usize);
+            }
+            bit_tags.push(bits);
+        }
+    }
+
+    if !use_bitsets {
+        return Ok(());
+    }
+
+    const NON_EXISTENT_IMG: u32 = u32::MAX;
+
+    let mut solution = Vec::<(u32, u32)>::new();
+    // later make this a random choice
+    match images.iter().find(|i| !i.vert).map(|i| i.id) {
+        Some(id) => {
+            solution.push((id, NON_EXISTENT_IMG));
+            images[id as usize].used = true;
+        },
+        None => {
+            solution.push((0, 1));
+            images[0].used = true;
+            images[1].used = true;
+        },
+    };
+
+    let mut total_score = 0;
+    loop {
+        let prev_slide = solution.last().unwrap();
+        let prev_slide_tags = if prev_slide.1 == NON_EXISTENT_IMG {
+            // this is a horizontal img
+            bit_tags[prev_slide.0 as usize].clone()
+        } else {
+            // it's two vertical images
+            let mut bits = bit_tags[prev_slide.0 as usize].clone();
+            bits.union_with(&bit_tags[prev_slide.1 as usize]);
+            bits
+        };
+
+
+        let mut best_image = (0, NON_EXISTENT_IMG);
+        for j in 0..n_images as usize {
+            // let's treat all images as horizontal
+            if images[j as usize].used { continue }
+
+            let score = get_score_bits(&prev_slide_tags, &bit_tags[j]);
+            best_image = std::cmp::max(best_image, (score, j as u32));
+        }
+        let (score, j) = best_image;
+        if j == NON_EXISTENT_IMG {
+            // we couldn't find a single image even, horizontal or vertical
+            break;
+        }
+
+        if !images[j as usize].vert {
+            // this is a horizontal image, add as is
+            solution.push((j, NON_EXISTENT_IMG));
+            images[j as usize].used = true;
+
+            total_score += score;
+        } else {
+            // we found a 'decent' vertical img, now we search for another 'decent' companion for it
+            let first_best_vertical_img_id = j;
+            
+            let mut best_image = (0, NON_EXISTENT_IMG);
+            for j in 0..n_images as usize {
+                if images[j as usize].used || j == first_best_vertical_img_id as usize { continue }
+                let mut curr_slide_tags = bit_tags[first_best_vertical_img_id as usize].clone();
+                curr_slide_tags.union_with(&bit_tags[j]);
+
+                let score = get_score_bits(&prev_slide_tags, &curr_slide_tags);
                 best_image = std::cmp::max(best_image, (score, j as u32));
             }
 
